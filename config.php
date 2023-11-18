6f97be8a77c73a33958dbdf6d010ebb58125f44d
leanwarrior <116770095+leanwarrior@users.noreply.github.com>
2023-11-17 18:09:49 -0600

    Data management ERP

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '1234' );             // Set database password
+  define( 'DB_NAME', 'motoprotect' );        // Set database name
+
+?>
